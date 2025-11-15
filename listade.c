d129017f1f7402d732a65aa4ada7ff672b2b8bbb
FaikyFigueiredo <contato.fabiohenriquefigueiredo@gmail.com>
2025-11-15 09:04:32 -0300

    Definition of functions

--- listade.c
+++ listade.c
@@ -1,6 +1,7 @@
 #include "listade.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void criar(ListaDE *l) {
   l->inicio = l->fim = NULL;
@@ -35,149 +36,60 @@ int inserir(ListaDE *l, Product* elem) {
 }
 
 void exibir(ListaDE *l) {
+  float total=0;
   No *aux;
   for (aux = l->inicio; aux != NULL; aux = aux->prox) {
-    printf("Produto %s", aux->prod->name);
-    printf("Categoria: %s", aux->prod->categoria);
-    printf("Quantidade: %dx\n", aux->prod->quantidade);
-    printf("Preco: R$%.2f\n", aux->prod->price);
-    printf("A Pagar: R$%.2f\n\n", aux->prod->price * aux->prod->quantidade);
+    printf("Produto - %s", aux->prod->name);
+    printf("  Categoria: %s", aux->prod->categoria);
+    printf("  Quantidade: %dx\n", aux->prod->quantidade);
+    printf("  Preco: R$%.2f\n", aux->prod->price);
+    printf("  A Pagar: R$%.2f\n\n", aux->prod->price * aux->prod->quantidade);
+    total += aux->prod->price * aux->prod->quantidade;
   }
+  printf("Total: %.2f\n", total);
+}
+void exibirCategoria(ListaDE* l,char name[]){
+  No* aux = l->inicio;
+  float total = 0;
+  printf("Exibindo itens da categoria %s\n", name);
+  for(;aux!=NULL;aux=aux->prox){
+    if (strcmp(aux->prod->categoria, name) == 0){
+      printf("Produto - %s", aux->prod->name);
+      printf("  Categoria: %s", aux->prod->categoria);
+      printf("  Quantidade: %dx\n", aux->prod->quantidade);
+      printf("  Preco: R$%.2f\n", aux->prod->price);
+      printf("  A Pagar: R$%.2f\n\n", aux->prod->price * aux->prod->quantidade);
+      total += aux->prod->price * aux->prod->quantidade;
+    }
+  }
+  printf("Total nessa categoria: %.2f\n", total);
 }
 
-int buscaPorElemento(ListaDE *l, int elem) {
+No* searchName(ListaDE *l, char name[]) {
   No *aux = l->inicio;
-  int pos = 0;
-
-  while (aux != NULL) {
-    //if (aux->dado == elem)
-    //  return pos;
-    pos++;
-    aux = aux->prox;
-  }
-  return -1;
+  for(;aux!=NULL && strcmp(name,aux->prod->name)!=0;aux = aux->prox);
+  return aux;
 }
-
-int buscaPorPosicao(ListaDE *l, int pos) {
-
-  //if (pos < 0 || pos >= tamanho(l))
-    return -1;
-
-  No *aux = l->inicio;
-  for (int i = 0; i < pos; i++) {
-    aux = aux->prox;
-  }
-  //return aux->dado;
-}
-
-int inserirInicio(ListaDE *l, int elem) {
-  //No *novo = (No *)malloc(sizeof(No));
-  //novo->dado = elem;
-  //novo->ant = novo->prox = NULL;
-  l->tamanho++;
-
-  // Quando a lista estiver vazia
-  //if (vazia(l)) {
-   // l->inicio = l->fim = novo;
-   // return 1;
-  //}
-
-  // Quando a lista nao estiver vazia
-  //novo->prox = l->inicio;
-  //l->inicio->ant = novo;
-  //l->inicio = novo;
-  return 1;
-}
-
-int inserirPosicao(ListaDE *l, int elem, int pos) {
-
-  // Posicao invalida
-  if (pos < 0 || pos > l->tamanho) {
+int removeName(ListaDE *l, char name[]) {
+  No* toRemove = searchName(l,name);
+  printf("Search valid\n");
+  if (toRemove == NULL)
     return 0;
-  }
-
-  // Inserir no inicio
-  if (pos == 0) {
-    return inserirInicio(l, elem);
-  }
-
-  // Inserir no final (logo apos o ultimo elemento)
-  if (pos == l->tamanho) {
-    //return inserir(l, elem);
-    return 1;
-  }
-
-  No *novo = (No *)malloc(sizeof(No));
-  No *aux = l->inicio;
-
-  for (int i = 0; i < pos - 1; i++) {
-    aux = aux->prox;
-  }
-
-  //novo->dado = elem;
-  novo->ant = aux;
-  novo->prox = aux->prox;
-  aux->prox->ant = novo;
-  aux->prox = novo;
-  l->tamanho++;
-  return 1;
-}
-
-int rem(ListaDE *l) {
-  No *temp = l->inicio;
-  l->inicio = l->inicio->prox;
-  l->inicio->ant = NULL;
-  free(temp);
-  return 1;
-}
-
-int removerInicio(ListaDE *l) {
-  // Se a rotina for chamada diretamente
-  // voce deve verificar se a lista estah vazia
-  No *temp = l->inicio;
-  l->inicio = l->inicio->prox;
-  l->inicio->ant = NULL;
-  free(temp);
+  if(toRemove == l->inicio)
+    l->inicio = toRemove->prox;
+  else
+    toRemove->ant->prox = toRemove->prox;
   l->tamanho--;
+  free(toRemove->prod);
+  free(toRemove);
   return 1;
 }
-
-int removerFim(ListaDE *l) {
-  // Se a rotina for chamada diretamente
-  // voce deve verificar se a lista estah vazia
-  No *temp = l->fim;
-  l->fim = l->fim->ant;
-  l->fim->prox = NULL;
-  free(temp);
-  return 1;
-}
-
-int remover(ListaDE *l, int elem) {
-  int pos = buscaPorElemento(l, elem);
-
-  if (pos == -1) {
+int update(ListaDE *l, char name[], Product* p){
+  No* toUpdate = searchName(l,name);
+  if (toUpdate == NULL)
     return 0;
-  }
-
-  if (pos == 0) {
-    return removerInicio(l);
-  }
-
-  if (pos == l->tamanho - 1) {
-    return removerFim(l);
-  }
-
-  // Posiciona no elemento anterior a ser removido
-  No *aux = l->inicio;
-  for (int i = 0; i < pos - 1; i++) {
-    aux = aux->prox;
-  }
-
-  No *temp = aux->prox;
-  aux->prox->prox->ant = aux;
-  aux->prox = aux->prox->prox;
+  Product* temp = toUpdate->prod;
+  toUpdate->prod = p;
   free(temp);
-  l->tamanho--;
   return 1;
-  
 }
--- main.c
+++ main.c
@@ -4,18 +4,19 @@
 #include "listade.h"
 
 void menu(){
-  printf("1 - Adicionar Produto\n");
+  printf("\n1 - Adicionar Produto\n");
   printf("2 - Remover Produto\n");
   printf("3 - Atualizar Produto\n");
   printf("4 - Visualizar Carrinho\n");
   printf("5 - Filtrar Produtos por Categoria\n");
-  printf("6 - Sair\n");
+  printf("6 - Sair\n\n");
 }
 int main(){
-  printf(">>> Bem-vindo ao Carrinho de Compras!\n\n");
+  printf(">>> Bem-vindo ao Carrinho de Compras!\n");
   ListaDE* l = (ListaDE*)malloc(sizeof(ListaDE));
   criar(l);
   while (1){
+    menu();
     int opt;
     printf("Selecione a opção:\n");
     scanf("%d", &opt);
@@ -29,7 +30,7 @@ int main(){
         fgets(p->name,40,stdin);
         printf("Digite a categoria do produto(Limpeza, Bebida, Alimento etc)\n");
         fgets(p->categoria,40,stdin);
-        printf("Digite a quantidade do produto\n");
+        printf("Digite a quantidade do produto a ser adicionado\n");
         scanf("%d",&quantidade);
         printf("Digite o preco do produto\n");
         scanf("%f",&price);
@@ -39,11 +40,30 @@ int main(){
       };
       break;
       case 2:{
-
+        char pName[40];
+        printf("Digite o nome do produto\n");
+        fgets(pName,40,stdin);
+        removeName(l,pName)? printf("Produto removido com sucesso\n") : printf("Produto não encontrado no carrinho\n");
       };
       break;
       case 3:{
-
+        char name[40];
+        int quantidade;
+        float price;
+        Product* p = (Product*)malloc(sizeof(Product));
+        printf("Digite o nome do produto a ser atualizado\n");
+        fgets(name,40,stdin);
+        printf("Digite o novo nome do produto\n");
+        fgets(p->name,40,stdin);
+        printf("Digite a nova categoria do produto(Limpeza, Bebida, Alimento etc)\n");
+        fgets(p->categoria,40,stdin);
+        printf("Digite a nova quantidade do produto\n");
+        scanf("%d",&quantidade);
+        printf("Digite o novo preco do produto\n");
+        scanf("%f",&price);
+        p->quantidade = quantidade;
+        p->price = price;
+        update(l,name,p)? printf("Produto atualizado com sucesso\n") : printf("Falha ao encontrar produto, utilize opcao 4 para verificar se o nome esta correto!\n");
       };
       break;
       case 4:{
@@ -51,7 +71,10 @@ int main(){
       };
       break;
       case 5:{
-
+        char name[40];
+        printf("Insira o nome da categoria a ser filtrada\n");
+        fgets(name,40,stdin);
+        exibirCategoria(l,name);
       };
       break;
       case 6:{return 0;};
